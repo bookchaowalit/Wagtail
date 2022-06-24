4555c0581185a136bc62d592f94ca8b747ed2d56
bookchaowalit <bookchaowalit@gmail.com>
2022-06-24 22:44:24 +0700

    dwqd

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-$7_r(va-d*taj8-y#q9u4y+gtnv9p(vcg*ro9dnetm8&ngw_rf"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
